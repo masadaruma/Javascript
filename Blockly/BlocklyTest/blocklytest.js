763a5babb84e2ab26541d5ce6369e0f9572a098d
masadaruma <masadaruma1225@gmail.com>
2022-04-30 02:27:24 +0900

    first commit

---
+++ Blockly/BlocklyTest/blocklytest.js
@@ -0,0 +1,454 @@
+Blockly.defineBlocksWithJsonArray(
+    [{
+        "type": "matrix",
+        "message0": "8X8 Matrix LED %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15 %16 %17 %18 %19 %20 %21 %22 %23 %24 %25 %26 %27 %28 %29 %30 %31 %32 %33 %34 %35 %36 %37 %38 %39 %40 %41 %42 %43 %44 %45 %46 %47 %48 %49 %50 %51 %52 %53 %54 %55 %56 %57 %58 %59 %60 %61 %62 %63 %64 %65 %66 %67 %68 %69 %70 %71 %72",
+        "args0": [
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "0",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "1",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "2",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "3",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "4",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "5",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "6",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "7",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "8",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "9",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "10",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "11",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "12",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "13",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "14",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "15",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "16",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "17",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "18",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "19",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "20",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "21",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "22",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "23",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "24",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "25",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "26",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "27",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "28",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "29",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "30",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "31",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "32",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "33",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "34",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "35",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "36",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "37",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "38",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "39",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "40",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "41",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "42",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "43",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "44",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "45",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "46",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "47",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "48",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "49",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "50",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "51",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "52",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "53",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "54",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "55",
+            "checked": true
+          },
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "field_checkbox",
+            "name": "56",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "57",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "58",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "59",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "60",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "61",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "62",
+            "checked": true
+          },
+          {
+            "type": "field_checkbox",
+            "name": "63",
+            "checked": true
+          }
+        ],
+        "inputsInline": false,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 330,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "loop",
+        "message0": "loop %1 %2",
+        "args0": [
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "input_statement",
+            "name": "NAME"
+          }
+        ],
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+      }]
+  );
+
+  var programArray = new Array();
+
+  Blockly.JavaScript['matrix'] = function(block) {
+    var checkbox_0 = block.getFieldValue('0') === 'TRUE';
+    var checkbox_1 = block.getFieldValue('1') === 'TRUE';
+    var checkbox_2 = block.getFieldValue('2') === 'TRUE';
+    var checkbox_3 = block.getFieldValue('3') === 'TRUE';
+    var checkbox_4 = block.getFieldValue('4') === 'TRUE';
+    var checkbox_5 = block.getFieldValue('5') === 'TRUE';
+    var checkbox_6 = block.getFieldValue('6') === 'TRUE';
+    var checkbox_7 = block.getFieldValue('7') === 'TRUE';
+    var checkbox_8 = block.getFieldValue('8') === 'TRUE';
+    var checkbox_9 = block.getFieldValue('9') === 'TRUE';
+    var checkbox_10 = block.getFieldValue('10') === 'TRUE';
+    var checkbox_11 = block.getFieldValue('11') === 'TRUE';
+    var checkbox_12 = block.getFieldValue('12') === 'TRUE';
+    var checkbox_13 = block.getFieldValue('13') === 'TRUE';
+    var checkbox_14 = block.getFieldValue('14') === 'TRUE';
+    var checkbox_15 = block.getFieldValue('15') === 'TRUE';
+    var checkbox_16 = block.getFieldValue('16') === 'TRUE';
+    var checkbox_17 = block.getFieldValue('17') === 'TRUE';
+    var checkbox_18 = block.getFieldValue('18') === 'TRUE';
+    var checkbox_19 = block.getFieldValue('19') === 'TRUE';
+    var checkbox_20 = block.getFieldValue('20') === 'TRUE';
+    var checkbox_21 = block.getFieldValue('21') === 'TRUE';
+    var checkbox_22 = block.getFieldValue('22') === 'TRUE';
+    var checkbox_23 = block.getFieldValue('23') === 'TRUE';
+    var checkbox_24 = block.getFieldValue('24') === 'TRUE';
+    var checkbox_25 = block.getFieldValue('25') === 'TRUE';
+    var checkbox_26 = block.getFieldValue('26') === 'TRUE';
+    var checkbox_27 = block.getFieldValue('27') === 'TRUE';
+    var checkbox_28 = block.getFieldValue('28') === 'TRUE';
+    var checkbox_29 = block.getFieldValue('29') === 'TRUE';
+    var checkbox_30 = block.getFieldValue('30') === 'TRUE';
+    var checkbox_31 = block.getFieldValue('31') === 'TRUE';
+    var checkbox_32 = block.getFieldValue('32') === 'TRUE';
+    var checkbox_33 = block.getFieldValue('33') === 'TRUE';
+    var checkbox_34 = block.getFieldValue('34') === 'TRUE';
+    var checkbox_35 = block.getFieldValue('35') === 'TRUE';
+    var checkbox_36 = block.getFieldValue('36') === 'TRUE';
+    var checkbox_37 = block.getFieldValue('37') === 'TRUE';
+    var checkbox_38 = block.getFieldValue('38') === 'TRUE';
+    var checkbox_39 = block.getFieldValue('39') === 'TRUE';
+    var checkbox_40 = block.getFieldValue('40') === 'TRUE';
+    var checkbox_41 = block.getFieldValue('41') === 'TRUE';
+    var checkbox_42 = block.getFieldValue('42') === 'TRUE';
+    var checkbox_43 = block.getFieldValue('43') === 'TRUE';
+    var checkbox_44 = block.getFieldValue('44') === 'TRUE';
+    var checkbox_45 = block.getFieldValue('45') === 'TRUE';
+    var checkbox_46 = block.getFieldValue('46') === 'TRUE';
+    var checkbox_47 = block.getFieldValue('47') === 'TRUE';
+    var checkbox_48 = block.getFieldValue('48') === 'TRUE';
+    var checkbox_49 = block.getFieldValue('49') === 'TRUE';
+    var checkbox_50 = block.getFieldValue('50') === 'TRUE';
+    var checkbox_51 = block.getFieldValue('51') === 'TRUE';
+    var checkbox_52 = block.getFieldValue('52') === 'TRUE';
+    var checkbox_53 = block.getFieldValue('53') === 'TRUE';
+    var checkbox_54 = block.getFieldValue('54') === 'TRUE';
+    var checkbox_55 = block.getFieldValue('55') === 'TRUE';
+    var checkbox_56 = block.getFieldValue('56') === 'TRUE';
+    var checkbox_57 = block.getFieldValue('57') === 'TRUE';
+    var checkbox_58 = block.getFieldValue('58') === 'TRUE';
+    var checkbox_59 = block.getFieldValue('59') === 'TRUE';
+    var checkbox_60 = block.getFieldValue('60') === 'TRUE';
+    var checkbox_61 = block.getFieldValue('61') === 'TRUE';
+    var checkbox_62 = block.getFieldValue('62') === 'TRUE';
+    var checkbox_63 = block.getFieldValue('63') === 'TRUE';
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'console.log("Matrix:")\n';
+    return code;
+  };
+  
+  Blockly.JavaScript['loop'] = function(block) {
+    var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'console.log("loop")\n';
+    return code;
+  };
+
